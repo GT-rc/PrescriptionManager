9c50d07d138c2b69fee09c271df6fa53c99e6a4b
Glynnis Thomas <glynnisthomas@gmail.com>
2017-04-26 23:51:37 -0500

    Updated Home View Templates; Database Migration

--- PrescriptionManager/src/PrescriptionManager/Data/Migrations/20170427044431_InitialNewModels.Designer.cs
+++ PrescriptionManager/src/PrescriptionManager/Data/Migrations/20170427044431_InitialNewModels.Designer.cs
@@ -8,9 +8,10 @@ using PrescriptionManager.Data;
 namespace PrescriptionManager.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20170427044431_InitialNewModels")]
+    partial class InitialNewModels
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.0.1")
---
+++ PrescriptionManager/src/PrescriptionManager/Data/Migrations/20170427044431_InitialNewModels.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Metadata;
+using PrescriptionManager.Models;
+
+namespace PrescriptionManager.Data.Migrations
+{
+    public partial class InitialNewModels : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Medication",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    ApplicationUserId = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true),
+                    Dosage = table.Column<int>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    Notes = table.Column<string>(nullable: true),
+                    TimeOfDay = table.Column<int>(nullable: false),
+                    TimesXDay = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Medication", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Medication_AspNetUsers_ApplicationUserId",
+                        column: x => x.ApplicationUserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.AddColumn<string>(
+                name: "FullName",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "PermissionLevel",
+                table: "AspNetUsers",
+                nullable: false,
+                defaultValue: PermissionLevel.Admin);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Medication_ApplicationUserId",
+                table: "Medication",
+                column: "ApplicationUserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FullName",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "PermissionLevel",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropTable(
+                name: "Medication");
+        }
+    }
+}
